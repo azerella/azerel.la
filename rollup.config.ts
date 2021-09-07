d2ac35549ee27bdde70d3bc2b661d2faeb9a4718
Adam Zerella <1501560+azerella@users.noreply.github.com>
2021-09-07 15:27:48 +0930

    Simple SCSS project setup

---
+++ rollup.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "rollup";
+import autoprefixer from "autoprefixer";
+import htmlTemplate from "rollup-plugin-generate-html-template";
+import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+  input: "src/styles.scss",
+  plugins: [
+    postcss({
+      extract: true,
+      sourceMap: !isProduction,
+      extensions: [".scss"],
+      use: ["sass"],
+      minimize: isProduction,
+      plugins: [autoprefixer()],
+    }),
+    htmlTemplate({
+      template: "public/index.html",
+      target: "dist/index.html",
+    }),
+    copy({
+      targets: [
+        {
+          src: "public/assets",
+          dest: "dist/",
+        },
+      ],
+    }),
+  ],
+  output: {
+    file: `dist/bundle.${isProduction ? "min.css" : "css"}`,
+  },
+});
